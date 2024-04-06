85d952690fb3e0e2da6dbf059d0f9b8947a1dbeb
aravindhraj <smkaravindh@gmail.com>
2024-04-06 06:42:56 +0530

    last

--- src/components/Productframe.js
+++ src/components/Productframe.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
-
-
+import { useState,useEffect } from "react";
+import axios from 'axios';
 const Productframe=()=>{
 
   const location = useLocation();
